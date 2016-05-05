6b79cd3259d9f501a1626f76ce488096af1290a8
icoon-git <sikharin10@gmail.com>
2016-05-05 15:37:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://admin:riderapp2016@ds013172.mlab.com:13172/rider-prod-parse',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
+  appId: process.env.APP_ID || 'yPCFkCNowXLmB32miK69pUsS7wLnuMkHoEEPsfFk',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'http://rider-parse-server.herokuapp.com',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
